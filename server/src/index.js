5805f5ac0f0a4285ae1df31c48aa7e5976a27c30
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-11-10 14:45:11 +0100

    Create route for users and ability to create user

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRouter = require('./routes/userRouter');
 
 dotenv.config({ path: './config.env' });
 
@@ -13,6 +14,8 @@ const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD,
 );
+
+app.use('/api/v1/users', userRouter);
 
 mongoose.connect(DB).then(() => {
   console.log('DB connection established');
