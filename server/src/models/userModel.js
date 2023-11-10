0fea67b18633aad66cd74e475765574e23c722da
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-11-10 21:20:13 +0100

    Create validator for passwordConfrim field and hash password

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -13,7 +14,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'User must confirm password'],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+    },
+    message: 'Passwords do not match',
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run if password was modified
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
