a1e0a238f02ed1570e77a73b4115f672710c2975
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-11-10 15:26:30 +0100

    Add controller to get user by id

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,30 @@
+const User = require('../models/userModel');
+const asyncHandler = require('express-async-handler');
+const AppError = require('../utils/appError');
+
+exports.getAllUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: users.length,
+    data: {
+      users,
+    },
+  });
+});
+
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(new AppError('No user found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user,
+    },
+  });
+});
--- server/src/routes/userRouter.js
+++ server/src/routes/userRouter.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
 router.post('/register', authController.signup);
+
+router.route('/').get(userController.getAllUsers);
+router.route('/:id').get(userController.getUser);
 
 module.exports = router;
