aaa5aed428e5dc3f7d2fffd9cf1d732d3b74ca3e
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-11-29 13:58:31 +0100

    Create middleware for upload images in create movie route

--- server/src/routes/movieRouter.js
+++ server/src/routes/movieRouter.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const movieController = require('../controllers/movieController');
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 const router = express.Router();
 
 router
   .route('/')
   .get(movieController.getAllMovies)
-  .post(movieController.createMovie);
+  .post(upload.single('file'), movieController.createMovie);
 
 router
   .route('/:slug')
