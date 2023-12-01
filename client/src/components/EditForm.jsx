352a6993092f91c57c0685f4980ec38a331d33da
Ajdin Husetovic <ahusetovic1@gmail.com>
2023-12-01 13:01:17 +0100

    Add delete and set watch status function

--- client/src/components/EditForm.jsx
+++ client/src/components/EditForm.jsx
@@ -1,18 +1,58 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 const EditForm = ({ toggleOpen, movie }) => {
+  const handleWatched = async () => {
+    try {
+      const response = await axios.patch(
+        `http://localhost:3000/api/v1/movies/${movie.slug}`,
+        {
+          isWatched: true,
+        }
+      );
+      console.log(response);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3000/api/v1/movies/${movie.slug}`
+      );
+      console.log(response);
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="edit-form">
       <div className="image">
         <img src={movie.imagePath} alt="" width="300px" height="444px" />
       </div>
       <div className="button-group">
-        <button id="watched-button">Set as watched</button>
-        <button id="delete-button">Delete Movie</button>
+        <button id="watched-button" onClick={handleWatched}>
+          Set as watched
+        </button>
+        <button id="delete-button" onClick={handleDelete}>
+          Delete Movie
+        </button>
       </div>
       <form>
         <div className="form-group">
           <label htmlFor="amh-rating">AMH Rating</label>
           <div className="input-wrapper">
-            <input type="number" />
+            <input
+              type="number"
+              min="1"
+              max="10"
+              placeholder="e.g. 5.4"
+              step="0.1"
+            />
             <button>Add rating</button>
           </div>
         </div>
